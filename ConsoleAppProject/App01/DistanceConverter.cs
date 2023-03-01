28f740726e532a0d4218e5909c4a24e173b870b2
kirandeepkaur2002 <115543802+kirandeepkaur2002@users.noreply.github.com>
2023-03-01 13:28:58 +0000

    added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,43 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// this app will convert the distance units 
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// kirandeep kaur 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        /// creating a method to call other methods within the class
+        /// </summary>
+        public void Run()
+        {
+
+        }
+        /// <summary>
+        /// prompt the user to enter the distance in miles
+        /// Input the miles as double number
+        /// </summary>
+
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
+
     }
 }
